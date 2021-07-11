b972d9aab7697c1ba039998d62b547e6611c0d3d
Aaron Silber <aaronlsilber@gmail.com>
2021-07-11 16:52:16 -0400

    customizations to tailwind.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { colors } = require("tailwindcss/defaultTheme");
+
 module.exports = {
   purge: {
     mode: "all",
@@ -7,12 +9,41 @@ module.exports = {
     },
   },
   theme: {
+    backgroundImage:  {
+      'hops': "url('/static/img/hops.jpg')",
+      'hops-gradient': "linear-gradient(to top, rgba(146, 187, 5, 1), rgba(146, 187, 5, 0) ), url('/static/img/hops.jpg')",
+    },
+    borderWidth: {
+      'thin': '1px',
+      '4': '4px',
+    },
     container: {
       center: true,
     },
-    extend: {
-      colors: {},
+    colors: {
+      lime: '#92BB05',
+      pine: '#284900',
+      orange: '#F19702',
+      white: colors.white,
+      black: '#252821',
+      beige: '#F4EFE4'
     },
+    fontFamily: {
+      'display': ['Alfa Slab One'],
+      'body': ['Roboto']
+    },
+    extend: {
+      boxShadow: {
+        'offset-white': '6px 6px 0 0 rgba(255, 255, 255, 0.4)',
+      },
+      fontSize: {
+        '7xl': '5rem',
+      },
+      padding: {
+        '10vh': '10vh',
+        '20vw': '20vw',
+      }
+    }
   },
   variants: {},
   plugins: [require("@tailwindcss/typography")],
